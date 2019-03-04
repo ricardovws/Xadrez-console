97df070a0633e1f3a73af9e82e4c11adc999deca
Ricardo <ricardovws@gmail.com>
2019-03-04 10:43:05 -0300

    Projeto iniciado, e também já fiz a classe Posicao

--- xandrez-console/Program.cs
+++ xandrez-console/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
-
+using tabuleiro;
 namespace xandrez_console
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Posicao P;
+
+            P = new Posicao(3, 4);
+
+            Console.WriteLine("Posição: " + P);
+
+            Console.WriteLine(); 
         }
     }
 }
