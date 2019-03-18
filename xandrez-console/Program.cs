caabfeed96dfdbc3a1c26eabfcaf1bbd40fd769b
Ricardo <ricardovws@gmail.com>
2019-03-18 19:23:07 -0300

    Comecando a implementar o tabuleiro, falta arrumar posicoes

--- xandrez-console/Program.cs
+++ xandrez-console/Program.cs
@@ -7,7 +7,7 @@ namespace xandrez_console
         static void Main(string[] args)
         {
             Tabuleiro tab = new Tabuleiro (8 , 8);
-
+            Tela.imprimirTabuleiro(tab);
             Console.WriteLine();
         }
     }
