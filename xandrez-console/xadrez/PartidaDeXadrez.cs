5d3675e64e0479a65a2cef8f7cff7ddba153d87e
Ricardo <ricardovws@gmail.com>
2019-07-29 21:38:42 -0300

    Jogada especial promocao

--- xandrez-console/xadrez/PartidaDeXadrez.cs
+++ xandrez-console/xadrez/PartidaDeXadrez.cs
@@ -150,7 +150,22 @@ namespace xandrez
             }
 
 
-            if(estaEmXeque(adversaria(jogadorAtual)))
+            Peca p = tab.peca(destino);
+
+            //#jogadaespecial promocao 
+            if (p is Peao)
+            {
+                if (p.cor == Cor.Branca && destino.linha == 0 || (p.cor == Cor.Preta && destino.linha==7))
+                {
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.cor);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
+
+            if (estaEmXeque(adversaria(jogadorAtual)))
             {
                 xeque = true;
             }
@@ -170,7 +185,7 @@ namespace xandrez
                 mudaJogador();
             }
 
-            Peca p = tab.peca(destino);
+            
 
             // #jogadaespecial en passant
             if (p is Peao && (destino.linha == origem.linha-2|| destino.linha==origem.linha+2))
